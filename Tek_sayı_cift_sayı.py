c2de4a5e2650f041b044c1a38f1883a176bd899f
busrasavran <72864078+busrasavran@users.noreply.github.com>
2020-11-04 12:26:21 +0300

    Add files via upload

---
+++ Tek_sayı_cift_sayı.py
@@ -0,0 +1,6 @@
+number=int(input("Enter a number: "))
+if number%2==0:  # %2 ifadesi burada 2 ile bölümünden kalanı veriyor.
+    print(number,"is an even number")
+
+else:
+    print(number,"is an odd number")
